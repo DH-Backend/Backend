002bc525d8274b716eebeb97176ccbe529262137
unknown <saezfvet@gmail.com>
2020-07-31 16:52:30 -0300

    ultimo

--- public/js front/productlist.js
+++ public/js front/productlist.js
@@ -1,10 +1,4 @@
 window.onload = () => {
-    let botonagregar = document.querySelector('.botonagregar');
 
-    for (cadauno of botonagregar) {
-        cadauno.onclick = () => {
-            const boton = this.getAttribute('data-AgregarId');
-        }
-    }
 
 }
